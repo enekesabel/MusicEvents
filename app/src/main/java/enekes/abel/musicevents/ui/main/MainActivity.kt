f7440cb9fd69fdfb5a8821fe47e8dee578b7c363
Énekes Ábel <enekes.abel@gmail.com>
2018-03-29 23:29:02 +0200

    basic views added

--- app/src/main/java/enekes/abel/musicevents/ui/main/MainActivity.kt
+++ app/src/main/java/enekes/abel/musicevents/ui/main/MainActivity.kt
@@ -7,7 +7,7 @@ import enekes.abel.musicevents.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainScreen {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +18,10 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+    }
+
+    override fun showEvents(artistName: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
 }
