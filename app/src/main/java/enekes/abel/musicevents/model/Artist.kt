d10b6496993579539513313b82c0950832d2bc53
Énekes Ábel <enekes.abel@gmail.com>
2018-04-21 00:10:43 +0200

    initial model classes added

--- app/src/main/java/enekes/abel/musicevents/model/Artist.kt
+++ app/src/main/java/enekes/abel/musicevents/model/Artist.kt
@@ -3,7 +3,22 @@ package enekes.abel.musicevents.model
 import com.orm.SugarRecord
 import enekes.abel.musicevents.network.model.ArtistData
 
-class Artist : SugarRecord<ArtistData>() {
+class Artist: SugarRecord<Artist> {
+
+    private var artistId: Int? = null
+    private var name: String? = null
+    private var url: String? = null
+    private var imageUrl: String? = null
+    private var facebookPageUrl: String? = null
+    private var isFavourite: Boolean = false
+
+    constructor(artistData: ArtistData){
+        this.artistId = artistData.id
+        this.url = artistData.url
+        this.imageUrl = artistData.imageUrl
+        this.name = artistData.name
+        this.facebookPageUrl = artistData.facebookPageUrl
+    }
 
 
 }
