6ebbd3557ea452c0b7f29003dc7d9718d534061e
Énekes Ábel <enekes.abel@gmail.com>
2018-05-10 21:35:01 +0200

    Event layout fiel for showing events in artist details renamed to artist_event_detail_event

--- app/src/main/java/enekes/abel/musicevents/ui/artists/artist_details/ArtistDetailsScreen.kt
+++ app/src/main/java/enekes/abel/musicevents/ui/artists/artist_details/ArtistDetailsScreen.kt
@@ -3,9 +3,6 @@ package enekes.abel.musicevents.ui.artists.artist_details
 import enekes.abel.musicevents.model.Artist
 import enekes.abel.musicevents.model.Event
 
-/**
- * Created by mobsoft on 2018. 03. 23..
- */
 interface ArtistDetailsScreen {
     fun showArtist(artist: Artist)
     fun showArtistEvents(events: List<Event>)
--- app/src/main/java/enekes/abel/musicevents/ui/events/EventRecyclerViewAdapter.kt
+++ app/src/main/java/enekes/abel/musicevents/ui/events/EventRecyclerViewAdapter.kt
@@ -7,7 +7,7 @@ import enekes.abel.musicevents.ui.utils.SingleLayoutAdapter
 
 class EventRecyclerViewAdapter(
         private val mValues: List<Event>)
-    : SingleLayoutAdapter(R.layout.fragment_event) {
+    : SingleLayoutAdapter(R.layout.artist_detail_event_item) {
 
 
     override fun getItemCount(): Int = mValues.size
