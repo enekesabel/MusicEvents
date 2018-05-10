4be2432dbd09872eaef045a89d00c537d0dd77bf
Énekes Ábel <enekes.abel@gmail.com>
2018-05-10 23:04:09 +0200

    event_item layout created and used to show favourite events

--- app/src/main/java/enekes/abel/musicevents/ui/events/EventListFragment.kt
+++ app/src/main/java/enekes/abel/musicevents/ui/events/EventListFragment.kt
@@ -28,6 +28,6 @@ class EventListFragment : AbstractBoundFragment<EventListScreen, FragmentEventLi
     }
 
     override fun showEventList(events: List<Event>) {
-        binding.eventsAdapter = ArtistDetailsEventRecyclerViewAdapter(events)
+        binding.eventsAdapter = EventRecyclerViewAdapter(events, activity)
     }
 }
---
+++ app/src/main/java/enekes/abel/musicevents/ui/events/EventRecyclerViewAdapter.kt
@@ -0,0 +1,35 @@
+package enekes.abel.musicevents.ui.events
+
+import android.content.Context
+import android.widget.ImageView
+import enekes.abel.musicevents.MusicEventsApplication
+import enekes.abel.musicevents.R
+import enekes.abel.musicevents.model.Event
+import enekes.abel.musicevents.ui.utils.BoundViewHolder
+import enekes.abel.musicevents.ui.utils.ImageManager
+import enekes.abel.musicevents.ui.utils.SingleLayoutAdapter
+
+
+class EventRecyclerViewAdapter(
+        private val mValues: List<Event>,
+        context: Context)
+    : SingleLayoutAdapter(R.layout.event_item) {
+
+    private val imageManager: ImageManager = ImageManager(context, MusicEventsApplication.IMAGE_DIR)
+
+    override fun onBindViewHolder(holder: BoundViewHolder,
+                                  position: Int) {
+        super.onBindViewHolder(holder, position)
+
+        val imageView = holder.itemView.findViewById<ImageView>(R.id.artist_image)
+        val artist = (holder.boundObject as Event).artist
+        if (artist?.imageFile != null) {
+            imageManager.loadImageFromFile(artist.imageFile!!, imageView)
+        }
+    }
+    override fun getItemCount(): Int = mValues.size
+
+    override fun getObjForPosition(position: Int): Any {
+        return mValues[position]
+    }
+}
